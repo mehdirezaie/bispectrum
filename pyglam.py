e47d75007d3021397be94b71112edd85bc001ef3
mehdirezaie <medirz90@icloud.com>
2022-04-07 13:23:04 -0500

    divide by the mean

--- pyglam.py
+++ pyglam.py
@@ -9,11 +9,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 from glob import glob
 from scipy.interpolate import RegularGridInterpolator as rgi
+from scipy.interpolate import interp1d
 
 mpl.use('Agg') 
 
 #--- input parameters ---
-is_bk = True
+is_bk = False
 ixmax  = 1000                        # maximum mock index
 KMIN, KMAX = 0.004, 0.296            # for applying a cut on k, to reduce the cov matrix dimension
 alphas = np.linspace(1.0, 1.8, 8000) # range of alphas
@@ -27,29 +28,13 @@ if is_bk:
 	f_bao  = lambda ix:f'/mnt/data1/BispectrumGLAM/BAO/Bk_CatshortV.0114.{ix:04d}.h5' 
 	f_nbao = lambda ix:f'/mnt/data1/BispectrumGLAM/noBAO/Bk_CatshortV.0114.{ix:04d}.h5'	
 else:
-	raise NotImplemented()
+	f_bao  = lambda ix:f'/mnt/data1/BispectrumGLAM/PowerspectrumGLAM/z0.50/BAO/Pk_CatshortV.0114.{ix:04d}.DAT' 
+	f_nbao = lambda ix:f'/mnt/data1/BispectrumGLAM/PowerspectrumGLAM/z0.50/noBAO/Pk_CatshortV.0114.{ix:04d}.DAT'	
 
 
 #--- helper functions
 def read_bkfile(input_file):
-	with open(input_file, 'r') as fl:
-		lines = fl.readlines()
-		bk = []
-		for line in lines:
-			k1, k2, k3, b = map(float, line.split('\t'))
-			bk.append([k1, k2, k3, b])
-	return np.array(bk)
-
-
-def read_pkfile(input_file):
-	with open(input_file, 'r') as fl:
-		lines = fl.readlines()
-		pk = []
-		for line in lines:
-			k1, b = map(float, line.split('\t'))
-			pk.append([k1, b])
-	return np.array(pk)
-
+	return np.loadtxt(input_file)
 
 def check_if_exists(input_file): # if a file exists
     return os.path.exists(input_file)
@@ -73,13 +58,17 @@ def read_ratios(ixmax): # read bispectra files and return ratios
 		if (check_if_exists(f_bao_i) and check_if_exists(f_nbao_i)):
 			bk1 = read_bkfile(f_bao_i)
 			bk2 = read_bkfile(f_nbao_i)
-			for i in range(3):assert np.array_equiv(bk1[:, i], bk2[:, i]), f"k%d does not match"%i
+			#for i in range(3):assert np.array_equiv(bk1[:, i], bk2[:, i]), f"k%d does not match"%i
 			bk1t += bk1[:, -1]
 			bk2t += bk2[:, -1]
 			bkr.append(bk1[:, -1])
 	#print(bk1)
 	nmocks = len(bkr)
-	k = bk1[:, :3]
+	if bk1.shape[1] > 2:
+		k = bk1[:, :3]
+	else:
+		k = bk1[:, 0]
+
 	bkr = np.array(bkr).T / (bk2t[:, np.newaxis]/nmocks)
 	bkrm = bk1t / bk2t
 	return (k, bkr, bkrm)
@@ -97,6 +86,7 @@ def read(bkr_file): # read bispectrum ratio file and return mean and cov matrix
 		bkrm = bkr_['bkrm']
 	return (k, bkr, bkrm)
 
+
 class Interpolate3D(object):
 	""" Interpolater for B(k1, k2, k3). Uses symmetry to fill the 3D matrix of B
 	"""
@@ -106,9 +96,9 @@ class Interpolate3D(object):
 		kmin = min(k[:, 2])
 		nk = int((kmax-kmin)/dk) + 1
 		kg = np.arange(kmin, kmax+dk/2, dk)
-		print(f'kmin={kmin:.3f}, kmax={kmax:.3f}, dk={dk:.3f}, nk={nk:d}')
+		#print(f'kmin={kmin:.3f}, kmax={kmax:.3f}, dk={dk:.3f}, nk={nk:d}')
 		br3d = np.zeros((nk, nk, nk))*np.nan
-		print(f'initialized 3D B(k): {br3d.shape} with k-grid = {kg}:')
+		#print(f'initialized 3D B(k): {br3d.shape} with k-grid = {kg}:')
 		for ki in range(k.shape[0]):
 			i = int((k[ki, 0]-kmin)/dk)
 			j = int((k[ki, 1]-kmin)/dk)
@@ -126,14 +116,33 @@ class Interpolate3D(object):
 	def __call__(self, *arrays):
 		return self.br3d_int(*arrays)
 
+
+class Interpolate1D(object):
+	""" Interpolater for Pk(k). Uses symmetry to fill the 1D array of P(k)
+	"""
+	def __init__(self, k, br):
+		kmax = max(k)
+		kmin = min(k)
+		dk = k[1]-k[0]
+		nk = k.size
+		#print(f'kmin={kmin:.3f}, kmax={kmax:.3f}, dk={dk:.3f}, nk={nk:d}')
+		self.br3d_int = interp1d(k, br, bounds_error=False) # turn this to True to see the error
+
+	def __call__(self, array):
+		return self.br3d_int(array)
+
 def get_alpha1sig(k, bkrm, br, br3d, kmax=KMAX, kmin=KMIN):
 	
 
 	# apply cut on k
 	#print(f'applying cut on k: {kmin:.3f} < k < {kmax:.3f}')
 	is_good = np.ones(k.shape[0], '?')
-	for i in range(3):is_good &= (k[:, i] > kmin) & (k[:, i] < kmax)
-	kg = k[is_good, :]
+	if len(k.shape) > 1:
+		for i in range(3):is_good &= (k[:, i] > kmin) & (k[:, i] < kmax)
+	else:
+		is_good &= (k > kmin) & (k < kmax)
+	
+	kg = k[is_good]
 	bg = bkrm[is_good]
 	nbins, nmocks = br[is_good, :].shape
 	hartlapf = (nmocks-1.0)/(nmocks-nbins-2.0)
@@ -176,15 +185,19 @@ def run():
 	print(f'br shape: {br.shape}')
 	print(f'bkrm shape: {bkrm.shape}')
 
-	# fill in the 3D matrix
-	br3d = Interpolate3D(k, bkrm)
+	if len(k.shape) > 1:
+		# fill in the 3D matrix
+		br_int = Interpolate3D(k, bkrm)
+	else:
+		br_int = Interpolate1D(k, bkrm)
+
 	#kmin_, kmax_ = 0.1, 0.175
 	#dalpha_ = get_alpha1sig(k, bkrm, br, br3d, kmax=kmax_, kmin=kmin_)
 	#print('dalpha', dalpha_)
 	alpha_1sig = []
 	for kmax_ in np.arange(KMIN, KMAX, 0.01):
 		for kmin_ in np.arange(KMIN, kmax_-0.02, 0.01):
-			dalpha_ = get_alpha1sig(k, bkrm, br, br3d, kmax=kmax_, kmin=kmin_)
+			dalpha_ = get_alpha1sig(k, bkrm, br, br_int, kmax=kmax_, kmin=kmin_)
 			alpha_1sig.append([kmin_, kmax_, dalpha_])
 			print('.', end='')
 
